79904a7c5302a6b7bcd629cedef6db1bd19249e9
Thu Vu Xuan <thuvuxuan@vccorp.vn>
2018-04-17 10:32:03 +0700

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="app-footer">
+        <a href="http://facebook.com/thuvx">Thu Vu Xuan</a> &copy; 2018.
+        <span className="float-right">Powered by CoreUI</span>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
